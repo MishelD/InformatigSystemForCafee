84541d4a4b31ce9442a6b391a6cb11083f8a3605
Mikhail Dolgushev <dolguschev.misha@ya.ru>
2023-06-30 17:30:11 +0300

    Update FinancialAccountingWindow.Designer.cs

--- ISFCprotopype/Windows/FinancialAccountingWindow.Designer.cs
+++ ISFCprotopype/Windows/FinancialAccountingWindow.Designer.cs
@@ -32,24 +32,24 @@ namespace ISFCprotopype
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FinancialAccountingWindow));
             this.BarPanel = new System.Windows.Forms.Panel();
+            this.exitButton = new ISFCprotopype.CustomElements.RoundButton();
+            this.listButton = new ISFCprotopype.CustomElements.RoundButton();
             this.financeFlowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.printButton = new ISFCprotopype.CustomElements.RoundButton();
             this.databaseCafeDataSet = new ISFCprotopype.databaseCafeDataSet();
             this.заказыBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.заказыTableAdapter = new ISFCprotopype.databaseCafeDataSetTableAdapters.ЗаказыTableAdapter();
             this.tableAdapterManager = new ISFCprotopype.databaseCafeDataSetTableAdapters.TableAdapterManager();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.printDialog1 = new System.Windows.Forms.PrintDialog();
-            this.printButton = new ISFCprotopype.CustomElements.RoundButton();
-            this.exitButton = new ISFCprotopype.CustomElements.RoundButton();
-            this.listButton = new ISFCprotopype.CustomElements.RoundButton();
             this.printDocument1 = new System.Drawing.Printing.PrintDocument();
             this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
             this.BarPanel.SuspendLayout();
             this.panel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.databaseCafeDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.заказыBindingSource)).BeginInit();
-            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // BarPanel
@@ -64,100 +64,6 @@ namespace ISFCprotopype
             this.BarPanel.Padding = new System.Windows.Forms.Padding(10, 11, 10, 11);
             this.BarPanel.Size = new System.Drawing.Size(68, 681);
             this.BarPanel.TabIndex = 0;
-            // 
-            // financeFlowLayoutPanel
-            // 
-            this.financeFlowLayoutPanel.AutoScroll = true;
-            this.financeFlowLayoutPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.financeFlowLayoutPanel.BackColor = System.Drawing.Color.Transparent;
-            this.financeFlowLayoutPanel.Dock = System.Windows.Forms.DockStyle.Top;
-            this.financeFlowLayoutPanel.Location = new System.Drawing.Point(0, 0);
-            this.financeFlowLayoutPanel.Margin = new System.Windows.Forms.Padding(0);
-            this.financeFlowLayoutPanel.Name = "financeFlowLayoutPanel";
-            this.financeFlowLayoutPanel.Padding = new System.Windows.Forms.Padding(12, 11, 12, 11);
-            this.financeFlowLayoutPanel.Size = new System.Drawing.Size(1196, 602);
-            this.financeFlowLayoutPanel.TabIndex = 2;
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.tableLayoutPanel1);
-            this.panel1.Controls.Add(this.financeFlowLayoutPanel);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(68, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1196, 681);
-            this.panel1.TabIndex = 3;
-            // 
-            // databaseCafeDataSet
-            // 
-            this.databaseCafeDataSet.DataSetName = "databaseCafeDataSet";
-            this.databaseCafeDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // заказыBindingSource
-            // 
-            this.заказыBindingSource.DataMember = "Заказы";
-            this.заказыBindingSource.DataSource = this.databaseCafeDataSet;
-            // 
-            // заказыTableAdapter
-            // 
-            this.заказыTableAdapter.ClearBeforeFill = true;
-            // 
-            // tableAdapterManager
-            // 
-            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
-            this.tableAdapterManager.UpdateOrder = ISFCprotopype.databaseCafeDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
-            this.tableAdapterManager.БлюдаTableAdapter = null;
-            this.tableAdapterManager.ЗаказыTableAdapter = this.заказыTableAdapter;
-            this.tableAdapterManager.ИнгредиентыTableAdapter = null;
-            this.tableAdapterManager.ПользователиTableAdapter = null;
-            this.tableAdapterManager.Состав_блюдаTableAdapter = null;
-            this.tableAdapterManager.Состав_заказаTableAdapter = null;
-            this.tableAdapterManager.СотрудникиTableAdapter = null;
-            // 
-            // tableLayoutPanel1
-            // 
-            this.tableLayoutPanel1.ColumnCount = 3;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 200F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Controls.Add(this.printButton, 1, 0);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 602);
-            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 1;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(1196, 79);
-            this.tableLayoutPanel1.TabIndex = 3;
-            // 
-            // printDialog1
-            // 
-            this.printDialog1.Document = this.printDocument1;
-            this.printDialog1.UseEXDialog = true;
-            // 
-            // printButton
-            // 
-            this.printButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(95)))), ((int)(((byte)(239)))));
-            this.printButton.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(95)))), ((int)(((byte)(239)))));
-            this.printButton.BorderColor = System.Drawing.Color.PaleVioletRed;
-            this.printButton.BorderRadius = 24;
-            this.printButton.BorderSize = 0;
-            this.printButton.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.printButton.FlatAppearance.BorderSize = 0;
-            this.printButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.printButton.Font = new System.Drawing.Font("Inter Medium", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.printButton.ForeColor = System.Drawing.Color.White;
-            this.printButton.Location = new System.Drawing.Point(498, 20);
-            this.printButton.Margin = new System.Windows.Forms.Padding(0, 20, 0, 11);
-            this.printButton.Name = "printButton";
-            this.printButton.Padding = new System.Windows.Forms.Padding(0, 10, 0, 10);
-            this.printButton.Size = new System.Drawing.Size(200, 48);
-            this.printButton.TabIndex = 0;
-            this.printButton.Text = "Печать";
-            this.printButton.TextColor = System.Drawing.Color.White;
-            this.printButton.UseVisualStyleBackColor = false;
-            this.printButton.Click += new System.EventHandler(this.printButton_Click);
             // 
             // exitButton
             // 
@@ -202,6 +108,100 @@ namespace ISFCprotopype
             this.listButton.TextColor = System.Drawing.Color.White;
             this.listButton.UseVisualStyleBackColor = false;
             // 
+            // financeFlowLayoutPanel
+            // 
+            this.financeFlowLayoutPanel.AutoScroll = true;
+            this.financeFlowLayoutPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.financeFlowLayoutPanel.BackColor = System.Drawing.Color.Transparent;
+            this.financeFlowLayoutPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.financeFlowLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.financeFlowLayoutPanel.Margin = new System.Windows.Forms.Padding(0);
+            this.financeFlowLayoutPanel.Name = "financeFlowLayoutPanel";
+            this.financeFlowLayoutPanel.Padding = new System.Windows.Forms.Padding(12, 11, 12, 11);
+            this.financeFlowLayoutPanel.Size = new System.Drawing.Size(1196, 602);
+            this.financeFlowLayoutPanel.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tableLayoutPanel1);
+            this.panel1.Controls.Add(this.financeFlowLayoutPanel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(68, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1196, 681);
+            this.panel1.TabIndex = 3;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 200F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.printButton, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 602);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1196, 79);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // printButton
+            // 
+            this.printButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(95)))), ((int)(((byte)(239)))));
+            this.printButton.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(95)))), ((int)(((byte)(239)))));
+            this.printButton.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.printButton.BorderRadius = 24;
+            this.printButton.BorderSize = 0;
+            this.printButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.printButton.FlatAppearance.BorderSize = 0;
+            this.printButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.printButton.Font = new System.Drawing.Font("Inter Medium", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.printButton.ForeColor = System.Drawing.Color.White;
+            this.printButton.Location = new System.Drawing.Point(498, 20);
+            this.printButton.Margin = new System.Windows.Forms.Padding(0, 20, 0, 11);
+            this.printButton.Name = "printButton";
+            this.printButton.Padding = new System.Windows.Forms.Padding(0, 10, 0, 10);
+            this.printButton.Size = new System.Drawing.Size(200, 48);
+            this.printButton.TabIndex = 0;
+            this.printButton.Text = "Печать";
+            this.printButton.TextColor = System.Drawing.Color.White;
+            this.printButton.UseVisualStyleBackColor = false;
+            this.printButton.Click += new System.EventHandler(this.printButton_Click);
+            // 
+            // databaseCafeDataSet
+            // 
+            this.databaseCafeDataSet.DataSetName = "databaseCafeDataSet";
+            this.databaseCafeDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // заказыBindingSource
+            // 
+            this.заказыBindingSource.DataMember = "Заказы";
+            this.заказыBindingSource.DataSource = this.databaseCafeDataSet;
+            // 
+            // заказыTableAdapter
+            // 
+            this.заказыTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.UpdateOrder = ISFCprotopype.databaseCafeDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            this.tableAdapterManager.БлюдаTableAdapter = null;
+            this.tableAdapterManager.ЗаказыTableAdapter = this.заказыTableAdapter;
+            this.tableAdapterManager.ИнгредиентыTableAdapter = null;
+            this.tableAdapterManager.ПользователиTableAdapter = null;
+            this.tableAdapterManager.Состав_блюдаTableAdapter = null;
+            this.tableAdapterManager.Состав_заказаTableAdapter = null;
+            this.tableAdapterManager.СотрудникиTableAdapter = null;
+            // 
+            // printDialog1
+            // 
+            this.printDialog1.Document = this.printDocument1;
+            this.printDialog1.UseEXDialog = true;
+            // 
             // printDocument1
             // 
             this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
@@ -230,14 +230,14 @@ namespace ISFCprotopype
             this.Name = "FinancialAccountingWindow";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Склад ингредиентов";
+            this.Text = "Финансовый учёт";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FinancialAccountingWindow_FormClosing);
             this.Load += new System.EventHandler(this.FinancialAccountingWindow_Load);
             this.BarPanel.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.databaseCafeDataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.заказыBindingSource)).EndInit();
-            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
